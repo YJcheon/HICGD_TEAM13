ffd259801ecc436503fd64f06e5a369938be65a3
Unknown <xogus1107@naver.com>
2019-11-11 16:45:31 +0900

    example
    add example

---
+++ Example/Cheonjokyo's-Last_Mission/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefab;
+    private Maze mazeInstance;
+
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame() {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+    }
+
+    private void RestartGame() {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
