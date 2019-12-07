458b57b9a0f3ab99f6ed1ea8ccaca59851f84051
Junyoung Park <carpe0308@naver.com>
2019-12-07 16:36:42 +0900

    add basic item trigger

---
+++ Cheonjokyo's-Last_Mission/Assets/ItemTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Player 아이템 획득.");
+
+            // TODO: 아이템 행동 구현 연결
+
+            Destroy(this.gameObject);
+        }
+    }
+}
