94fb40b2a6b4f52da2644dcbe06ecc4ca9840531
Junyoung Park <carpe0308@naver.com>
2019-12-07 19:56:42 +0900

    add HealthPointGetter

---
+++ Cheonjokyo's-Last_Mission/Assets/HealthPointGetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPointGetter : MonoBehaviour
+{
+    private GameManager manager;
+    private Text text;
+    private double HP;
+
+    // Update is called once per frame
+    void Start()
+    {
+        GameObject managerObject = GameObject.FindWithTag("GameController");
+        manager = managerObject.GetComponent<GameManager>();
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        HP = manager.player.HP;
+        text.text = string.Format("{0:00.00}", HP);
+    }
+}
