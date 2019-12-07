5af352fb9a24067e19f8bff1a2a3c1eee5d380f6
Junyoung Park <carpe0308@naver.com>
2019-12-07 17:54:21 +0900

    add timer

---
+++ Cheonjokyo's-Last_Mission/Assets/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public double initialTime;
+    private double restTime { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restTime = initialTime;
+    }
+
+    void initialize()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        restTime -= Time.deltaTime;
+        Debug.Log("timer: " + restTime + " seconds");
+    }
+
+    public void increase(float seconds)
+    {
+        restTime += seconds;
+    }
+
+    public void decrease(float seconds)
+    {
+        restTime -= seconds;
+    }
+}
